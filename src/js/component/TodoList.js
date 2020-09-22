05f296ddcab44223aed5d94d0a59167590bb66b5
jgustavoj <gjime011@fiu.edu>
2020-09-22 01:23:11 +0000

    comments

--- src/js/component/TodoList.js
+++ src/js/component/TodoList.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+//{} means that this is not a dafault import/exporting
 import Todo from "./Todo";
 
 const TodoList = () => {
@@ -9,16 +10,26 @@ const TodoList = () => {
 		{ label: "walk dog" }
 	]);
 	const handleChange = e => {
-		setSingleTodo({ label: e.target.value });
+		setSingleTodo({ label: e.target.value, done: false });
 	};
+
+	//e.target.value gives you the value of the input field on change (typing on the input field)
+
 	const handleClick = e => {
 		setTodos([...todos, singleTodo]);
 	};
+
+	//set singleTodo {}
 	const deleteTodo = index => {
 		const newTodos = [...todos];
 		newTodos.splice(index, 1);
 		setTodos(newTodos);
 	};
+
+	// the state is immutable - you can never change the state.
+	// We copy the state we can manipulate the copy of the state then we
+	// push the new todo
+
 	return (
 		<>
 			<form onSubmit={e => e.preventDefault()}>
@@ -31,6 +42,8 @@ const TodoList = () => {
 				<button onClick={handleClick}> Save </button>
 			</form>
 			{todos.map((value, index) => (
+				//map manipulates an array, if you want to change anything to an array you use map method
+				//creating a new a array from the old array
 				<Todo
 					todo={value.label}
 					key={index}
@@ -57,3 +70,17 @@ export default TodoList;
 //     newTodos.push(singleTodo)
 //     setTodos (newTodos)
 // }
+
+//React is a one page application if you refresh the site then you might lose information in your state
+//we dont want that
+
+//another way to delete an item from list
+
+// const [todos, setTodos] = useState (["Run", "walk", "sleep"]);
+// const deletetask = (ind) => {
+//     var newTodos = todos.filter ((item, i) => {return ind !=== i});
+// }
+
+// return
+
+// <button onClick = {() = > deletetask(1)}>Delete</button>
